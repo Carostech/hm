a833f82b4635a88b8421a58fdd5fd6d495027923
CAkinyi <akinyicarol66@gmail.com>
2018-09-09 22:31:31 +0300

    User logout

--- hotelweb/urls.py
+++ hotelweb/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
                 path('register/', views.user_register, name='user_register'),
                 path('login/', views.user_login, name='user_login'),
+                path('logout/', views.user_logout, name='user_logout'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
